9e95d1a971f3ff2c2a7a9e8ea6fdffe0282daa54
LeonardoMiguel04 <159266494+LeonardoMiguel04@users.noreply.github.com>
2024-09-12 12:14:56 -0300

    Add files via upload

---
+++ calcs.js
@@ -0,0 +1,11 @@
+function calcula(opera){
+    let a=parseFloat(document.getElementById('a').value);
+    let b=parseFloat(document.getElementById('b').value);
+    let c=parseFloat(document.getElementById('c').value);
+    let resul=0
+}
+
+delta = (b*b)*(-4*a*c);
+delta=Math.sqrt(delta);
+let x1 = (-b+delta)/(2*a);
+let x2 = (-b-delta)/(2*a);
