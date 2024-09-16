2508967536478d4076ec24be5ea280852e89c595
LeonardoMiguel04 <159266494+LeonardoMiguel04@users.noreply.github.com>
2024-09-16 09:24:07 -0300

    Add files via upload

--- calcs.js
+++ calcs.js
@@ -1,11 +1,18 @@
-function calcula(opera){
+function calcula(){
     let a=parseFloat(document.getElementById('a').value);
     let b=parseFloat(document.getElementById('b').value);
     let c=parseFloat(document.getElementById('c').value);
-    let resul=0
-}
+    let delta = (b*b)-(4*a*c);
+    let rdelta=Math.sqrt(delta);
 
-delta = (b*b)*(-4*a*c);
-delta=Math.sqrt(delta);
-let x1 = (-b+delta)/(2*a);
-let x2 = (-b-delta)/(2*a);
+    if (delta == 0){
+        alert('sem resolução quando delta é igual a 0');
+    }else if (delta < 0){
+        let x1 = (-b)/(2*a);
+        document.getElementById('resul').innerText = 'Resultado: ' + x1;
+    }else{
+        let x1 = (-b+rdelta)/(2*a);
+        let x2 = (-b-rdelta)/(2*a);
+        document.getElementById('resul').innerText = 'Resultado: ' + x1 + ' e ' + x2;
+    }
+}
